0ace7d1a49244fe2dcdbcf7fbe552a58deda95d7
cip <cip@gmx.at>
2011-07-03 23:47:37 +0200

    Translation template for italian language added.

---
+++ wikionboard_it.ts
@@ -0,0 +1,255 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>WikiOnBoard</name>
+    <message>
+        <location filename="wikionboard.ui" line="20"/>
+        <source>WikiOnBoard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.ui" line="102"/>
+        <source>Load Article (Progress bar </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.ui" line="125"/>
+        <location filename="wikionboard.ui" line="130"/>
+        <location filename="wikionboard.ui" line="135"/>
+        <source>Open eBook</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.ui" line="140"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="280"/>
+        <source>Open Zimfile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="284"/>
+        <source>Download Zimfile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="287"/>
+        <source>Goto Homepage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="290"/>
+        <source>About current Zimfile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="293"/>
+        <location filename="wikionboard.cpp" line="1143"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="296"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="303"/>
+        <source>Search Article</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="325"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="333"/>
+        <source>Open Article</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="347"/>
+        <source>Switch to index page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="355"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="360"/>
+        <source>Toggle Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="366"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="370"/>
+        <source>Zoom in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="371"/>
+        <source>Zoom out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="408"/>
+        <source>Error on opening zim file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="472"/>
+        <source>Error: article not found. (URL: %1 )</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="855"/>
+        <source>Open link in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="856"/>
+        <source>[TRANLATOR]Explain that may not work if browser running.</source>
+        <comment>only displayed if self_signed or QT&lt;4.7.0</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="872"/>
+        <source>[TRANSLATOR] Explain that link %1 clicked in article is not contained in ebook and needs to be opened in webrowser. Ask if ok.%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="958"/>
+        <source>Choose eBook in zim format to open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="959"/>
+        <source>eBooks (*.zim *.zima)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="980"/>
+        <source>http://openzim.org/ZIM_File_Archive</source>
+        <comment>Change link to page with localized zim files if available.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="982"/>
+        <source>Download ZIM file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="983"/>
+        <source>Open a webbrowser to download zim files from %1?
+Note that zim files may be very large and thus it can beexpensive to download one over the mobile network. You should consider download from a desktop systemand transfer the file later to the memory card of your phone.
+Furthermore, note that current Symbian phones do not support files whichare larger than 2 GB. You cannot download such files directly on the phone,but you have to download on a PC and follow the instructions onto split them.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="991"/>
+        <source>http://wiki.github.com/cip/WikiOnBoard/</source>
+        <comment>Change link to localized webpage if/when available.</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1017"/>
+        <source>http://wiki.github.com/cip/WikiOnBoard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1019"/>
+        <source>Goto homepage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1021"/>
+        <source>Open a webbrowser to show WikiOnBoard&apos;s homepage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1055"/>
+        <source>Not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1075"/>
+        <source>About Current Zimfile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1078"/>
+        <source>No zim file is currently opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1082"/>
+        <source>Current Zim File: %1
+Articles : %2, Images: %3, Categories: %4
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1092"/>
+        <source>Title: %1
+Creator: %2
+Date: %3
+Source: %4
+Description: %5
+Language: %6
+Relation: %7
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1109"/>
+        <source>UUID: %1
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1138"/>
+        <source>application is self-signed</source>
+        <comment>only displayed if application is self-signed</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1144"/>
+        <source>WikiOnBoard %1
+Author: %2
+Uses zimlib (openzim.org) and liblzma.
+Build date: %3
+%4
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1151"/>
+        <source>Christian Puehringer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1191"/>
+        <location filename="wikionboard.cpp" line="1219"/>
+        <source>Options</source>
+        <comment>Option menu</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="wikionboard.cpp" line="1195"/>
+        <location filename="wikionboard.cpp" line="1222"/>
+        <source>Help</source>
+        <comment>Help menu</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
