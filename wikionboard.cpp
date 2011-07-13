fe7f6100b87e86cf8ed0708f18e931a564944b5f
cip <cip@gmx.at>
2011-07-13 22:00:19 +0200

    Added GPL license header

--- wikionboard.cpp
+++ wikionboard.cpp
@@ -1,32 +1,18 @@
-/****************************************************************************
- **
- ** Trolltech hereby grants a license to use the Qt/Eclipse Integration
- ** plug-in (the software contained herein), in binary form, solely for the
- ** purpose of creating code to be used with Trolltech's Qt software.
- **
- ** Qt Designer is licensed under the terms of the GNU General Public
- ** License versions 2.0 and 3.0 ("GPL License"). Trolltech offers users the
- ** right to use certain no GPL licensed software under the terms of its GPL
- ** Exception version 1.2 (http://trolltech.com/products/qt/gplexception).
- **
- ** THIS SOFTWARE IS PROVIDED BY TROLLTECH AND ITS CONTRIBUTORS (IF ANY) "AS
- ** IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- ** TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- ** PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
- ** OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- ** EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- ** PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- ** PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- ** LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- ** NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- ** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
- **
- ** Since we now have the GPL exception I think that the "special exception
- ** is no longer needed. The license text proposed above (other than the
- ** special exception portion of it) is the BSD license and we have added
- ** the BSD license as a permissible license under the exception.
- **
- ****************************************************************************/
+/*  WikiOnBoard
+    Copyright (C) 2011  Christian Puehringer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 //TODO: Find some replacement for hourglass. (Not working anymore as mouse cursor disabled)
 //TODO fix curso in textedit (don't move on left/right). Consider however before the
